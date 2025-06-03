b9c1d27ebe5ee1b8388e859ec3a1a6f0ad177946
Sahar Mor <saharhashai@gmail.com>
2025-06-03 16:29:57 +0200

    Remove count_and_speak.py and count_letters.py scripts; add mouse movement to bug_hunter.py

--- hello.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-def print_hello(count):
-    """Print 'Hello' a specified number of times."""
-    for i in range(count):
-        print(f"Hello")
-
-if __name__ == "__main__":
-    print_hello(6) 
